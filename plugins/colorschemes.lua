4e1e4c75cd48d47ae6383281b3a56248de6bdd5c
Vince Brown <vince@vincebrown.me>
2023-10-15 07:08:11 -0700

    Initial Commit

---
+++ plugins/colorschemes.lua
@@ -0,0 +1,12 @@
+return {
+  { "EdenEast/nightfox.nvim" },
+  { "sainnhe/everforest" },
+  { "catppuccin/nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "rose-pine/neovim" },
+  { "luisiacc/gruvbox-baby" },
+  { "savq/melange-nvim" },
+  { "marko-cerovac/material.nvim" },
+  { "nyoom-engineering/oxocarbon.nvim" },
+  { "NLKNguyen/papercolor-theme" },
+}
